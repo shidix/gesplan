a0967495e093c1febd12ce15f7e6dd5688bceb8f
Zeben Perez <zeben@vps-d65b23f4.vps.ovh.net>
2025-06-19 10:07:56 +0000

    ajuste gestor externo

--- gestion/models.py
+++ gestion/models.py
@@ -294,7 +294,7 @@ class WasteInFacility(models.Model):
 
     @property
     def external_manager(self):
-        if seft.waste != None and self.waste.external_manager != None:
+        if self.waste != None and self.waste.external_manager != None:
             return self.waste.external_manager
 
         fwm = FacilityWasteManager.objects(waste=self.waste, facility=self.facility).first()
